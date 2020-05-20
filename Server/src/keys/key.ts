4a7fc5f0ad0e3cbf70308ad49bf454e2484feb24
Erik Flores <41808911+erflod5@users.noreply.github.com>
2020-05-20 06:12:43 -0600

    Update key.ts

--- Server/src/keys/key.ts
+++ Server/src/keys/key.ts
@@ -1,8 +1,8 @@
 let keyMongo = {
-    server : process.env.DB_SERVER || '3.23.132.171:27017',
-    database : process.env.DB_DATABASE || 'uSocial',
-    user : process.env.DB_USER || 'erflod1',
-    pass : process.env.DB_PASS || 'usocial123'
+    server : process.env.DB_SERVER || '',
+    database : process.env.DB_DATABASE || '',
+    user : process.env.DB_USER || '',
+    pass : process.env.DB_PASS || ''
 }
 
 export default keyMongo;
